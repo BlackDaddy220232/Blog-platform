b3fbfa43a0d5e16a1441f0362d78b644318dc3bd
BlackDaddy220232 <sashamynzul@gmail.com>
2024-07-15 20:55:11 +0300

    init project

---
+++ blogPlatform/src/main/java/platform/blogPlatform/BlogPlatformApplication.java
@@ -0,0 +1,13 @@
+package Test.blogPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPlatformApplication.class, args);
+	}
+
+}
