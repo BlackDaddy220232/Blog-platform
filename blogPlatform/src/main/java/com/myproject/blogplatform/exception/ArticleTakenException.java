f1cd31f53e309d9de33f3b9221d776ee5bc9f3b4
BlackDaddy220232 <sashamynzul@gmail.com>
2024-07-17 22:37:34 +0300

    feat: Exceptions

---
+++ blogPlatform/src/main/java/com/myproject/blogplatform/exception/ArticleTakenException.java
@@ -0,0 +1,7 @@
+package com.myproject.blogplatform.exception;
+
+public class ArticleTakenException extends RuntimeException {
+    public ArticleTakenException(String message){
+        super(message);
+    }
+}
